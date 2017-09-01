2e469706d93f8ad8277b8a413b848f24ceab779b
Padad5 <31539286+Padad5@users.noreply.github.com>
2017-09-01 13:30:20 +0100

    Add files via upload

---
+++ Backup-DNSZones.ps1
@@ -0,0 +1,2 @@
+﻿$zones = Get-DnsServerZone -ComputerName DC1 | where {$_.IsDsIntegrated -ne 'True' }
+$zones | Select ZoneName,ZoneType,AllowUpdate,@{Name=”MasterServers”;Expression={$_.MasterServers}},@{Name=”SecondaryServers”;Expression={$_.SecondaryServers}},IsDsIntegrated | ft #| Export-Csv DNS.csv -NoTypeInformation
---
+++ Restore-DNSZones.ps1
@@ -0,0 +1,31 @@
+﻿$DNSServer = "DC1"
+$Zones = Import-Csv .\DNS.CSV
+
+$Zones | foreach {
+    $Zone=$_.name
+    $Master=$_.MasterServers
+    $Second=$_.SecondaryServers
+
+    #Write-Output Zone: $Zone
+    #Write-Output Master: $Master
+    #Write-Output Second: $Second
+
+    Switch ($_.ZoneType) {
+       
+        2 {$cmd1=”dnscmd {0} /ZoneAdd {1} /secondary {2}” -f $DNSServer,$Zone,$Master 
+            $cmd2="dnscmd {0} /ZoneResetSecondaries {1} {2}" -f $DNSServer,$Zone,$Second
+          }
+        3 {$cmd1=”dnscmd {0} /ZoneAdd {1} /stub {2}” -f $DNSServer,$Zone,$Master }
+        4 {$cmd1=”dnscmd {0} /ZoneAdd {1} /forwarder {2}” -f $DNSServer,$Zone,$Master }
+    }
+
+    Write-Output $cmd1
+    Write-Output $cmd2
+    
+    cmd /c $cmd1
+    cmd /c $cmd2
+
+    Clear-Variable cmd2
+
+
+}
